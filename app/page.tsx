09f12c2f5fb2281f4fe02d8ebecd0c5dd4f672f2
MazaaLazaa <146481617+MazaaLazaa@users.noreply.github.com>
2025-02-15 08:52:56 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`Welcome to my website! I’m Djim, a passionate professional at the crossroads of coding, marketing, and analytics. I love turning data into insights, building scalable solutions, and crafting strategic campaigns that drive real impact. Explore my work and let’s connect!`}
+        {` I’m Djim, a passionate professional at the crossroads of coding, marketing, and analytics. I love turning data into insights, building scalable solutions, and crafting strategic campaigns that drive real impact. Explore my work and let’s connect!`}
       </p>
       <div className="my-8">
         <BlogPosts />
